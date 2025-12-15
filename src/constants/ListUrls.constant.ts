b2b23bb30c9f98a2f84af10a656e436d32cfa8ba
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-12-15 09:05:03 -0500

    Iniciando seccion 4 - dinamismo a sitio estatico

---
+++ src/constants/ListUrls.constant.ts
@@ -0,0 +1,16 @@
+import type {ListUrlsInterface} from "@/interfaces/listUrls.interface.ts";
+
+export const ListUrlsConstant: ListUrlsInterface[] = [
+    {
+        url: '/',
+        title: 'Inicio',
+    },
+    {
+        url: '/pokemons/1',
+        title: 'Paginado',
+    },
+    {
+        url: '/favorites',
+        title: 'Favoritos',
+    }
+]
---
+++ src/interfaces/listUrls.interface.ts
@@ -0,0 +1,4 @@
+export interface ListUrlsInterface {
+    url: string;
+    title: string;
+}
